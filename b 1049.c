9b208094c9c7143deb01fcbf3d64bef28fccd860
Munna <shakibhassanmunna777@gmail.com>
2025-01-21 00:13:33 +0600

    Beecrowd

--- b 1049.c
+++ b 1049.c
@@ -9,37 +9,37 @@ int main()
         printf("aguia\n");
     }
 
-    if(W1[100] == 'vertebrado' && W2[100] =='ave' && W3[100] == 'onivoro' )
+    if(W1[0] == 'v' && W2[0] =='a' && W3[0] == 'o' )
     {
         printf("pomba\n");
     }
 
-    if(W1[100] == 'vertebrado' && W2[100] =='mamifiro' && W3[100] == 'onivoro' )
+    if(W1[0] == 'v' && W2[0] =='m' && W3[0] == 'o' )
     {
         printf("homem\n");
     }
 
-    if(W1[100] == 'vertebrado' && W2[100] =='mamifiro' && W3[100] == 'herbivoro' )
+    if(W1[0] == 'v' && W2[0] =='m' && W3[0] == 'h' )
     {
         printf("vaca\n");
     }
 
-    if(W1[100] == 'invertebrado' && W2[100] =='inseto' && W3[100] == 'hematofago' )
+    if(W1[0] == 'i' && W2[2] =='s' && W3[0] == 'h' )
     {
         printf("pulga\n");
     }
 
-    if(W1[100] == 'invertebrado' && W2[100] =='inseto' && W3[100] == 'herbivoro' )
+    if(W1[0] == 'i' && W2[2] =='s' && W3[2] == 'r' )
     {
         printf("lagarta\n");
     }
 
-    if(W1[100] == 'invertebrado' && W2[100] =='anelideo' && W3[100] == 'hematofago' )
+    if(W1[0] == 'i' && W2[1] =='n' && W3[0] == 'h' )
     {
         printf("sanguessuga\n");
     }
 
-    if(W1[100] == 'invertebrado' && W2[100] =='anelideo' && W3[100] == 'onivoro' )
+    if(W1[0] == 'i' && W2[1] =='n' && W3[0] == 'o' )
     {
         printf("minhoca\n");
     }
