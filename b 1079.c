e8f6bf89e96c4dca5724a88b01bb15f5cd08a221
Munna <shakibhassanmunna777@gmail.com>
2025-01-21 02:43:37 +0600

    Beecrowd

--- b 1079.c
+++ b 1079.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
-
-int main()
+#include <stdio.h>
+int main() 
 {
-    int n,i;
-    float a,b,c,sum,avg;
-    scanf("%d",&n);
+    int N;
+    float num1, num2, num3, Average;
 
-    for(i=0;i<n;i++)
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) 
     {
-        scanf("%f %f %f",&a,&b,&c);
-        sum=a*2+b*3+c*5;
-        avg=sum/(2+3+5);
-        printf("%.1f\n",avg);
+        scanf("%f %f %f", &num1, &num2, &num3);
+        Average = (num1 * 2 + num2 * 3 + num3 * 5) / 10;
+        printf("%.1f\n",Average);
     }
     return 0;
 }
