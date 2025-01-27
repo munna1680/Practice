1666b8655d2374e204f5b8bcb7edf2997ce4156b
Munna <shakibhassanmunna777@gmail.com>
2025-01-28 04:19:31 +0600

    beecrowd

---
+++ b 1099.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, x, y;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d %d", &x, &y);
+        if (x > y) 
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        int sum = 0;
+        for (int j = x + 1; j < y; j++) 
+        {
+            if (j % 2 != 0) {
+                sum += j;
+            }
+        }
+        printf("%d\n", sum);
+    }
+
+    return 0;
+}
--- b 1115.c
+++ b 1115.c
@@ -4,7 +4,7 @@ int main()
     int a,b;
     while(1)
     {
-        scanf("%d%d", &a, &b);
+        scanf("%d %d", &a, &b);
         if(a==0 || b==0)
             break;
         else if(a>0 && b>0)
