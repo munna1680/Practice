cf43338488ab2df9af6ed19df429bb9c6629be1f
Munna <shakibhassanmunna777@gmail.com>
2025-01-29 17:51:26 +0600

    normal

---
+++ b 1146.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x;
+    while (1) 
+    {
+        scanf("%d", &x);
+        if (x == 0) 
+        break;
+        for (int i = 1; i <= x; i++) 
+        {
+            printf("%d%s", i, (i == x) ? "\n" : " ");
+        }
+    }
+    return 0;
+}
