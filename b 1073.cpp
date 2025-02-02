9ebdc4724927a0722bc64000a7fb99419aba0dd6
Munna <shakibhassanmunna777@gmail.com>
2025-02-02 23:50:13 +0600

    hudai

--- b 1073.cpp
+++ b 1073.cpp
@@ -2,11 +2,9 @@
 #include <math.h>
 int main() 
 {
-    int N;
-
-    scanf("%d", &N);
-
-    for (int i = 2; i <= N; i += 2) 
+    int n;
+    scanf("%d", &n);
+    for (int i = 2; i <= n; i += 2) 
     {
         printf("%d^2 = %d\n", i, i * i);
     }
