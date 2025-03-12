fb60e80db6c635d406fbe5c761ded1a3605cf28b
Munna <shakibhassanmunna777@gmail.com>
2025-03-13 05:10:57 +0600

    1

---
+++ Function/even or odd.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void hello_f()
+{
+    printf("Hello! I am from inside of a function\n");
+}
+
+void val_f(int a)
+{
+    if(a % 2 == 0)
+    {
+        printf("Even\n");
+    }
+    else 
+    {
+        printf("odd\n");
+    }
+
+}
+
+
+
+int main()
+{
+    hello_f();
+    
+    printf("\n");
+
+    val_f(100);
+
+    return 0;
+}
