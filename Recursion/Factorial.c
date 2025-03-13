7d94bcb0538dec363d5311c61befcad21583603a
Munna <shakibhassanmunna777@gmail.com>
2025-03-13 16:11:25 +0600

    1

---
+++ Recursion/Factorial.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int fact(int a)
+{
+    if(a == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int Fact = a * fact(a - 1);
+        return Fact;
+    }
+    
+}
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+
+    int factorial = fact(x);
+
+    printf("%d\n",factorial);
+
+
+
+    return 0;
+}
