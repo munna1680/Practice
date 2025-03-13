6dabdca091626754433c6e2758a8c8decce71971
Munna <shakibhassanmunna777@gmail.com>
2025-03-14 01:09:03 +0600

    4

--- Recursion/power of a number.c
+++ Recursion/power of a number.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
+#include<math.h>
 
+int power(int a, int b)
+{
+    if(b == 0)
+    return 1;
 
+    else
+    return (a *power(a,b-1));
+
+}
 
 
 int main()
 {
-    
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    power(a,b);
+    printf("Power of a number (%d^%d) = %d\n",a,b,power(a,b));
+
+
+    return 0;
 }
